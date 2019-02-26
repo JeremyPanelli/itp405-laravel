2280dda9b81792f375f6f728b89d15ccf71c435b
JEREMY-LAPTOP\Jeremy <panelli@usc.edu>
2019-02-26 12:39:29 -0800

    merge didn't work before

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
-<<<<<<< HEAD
-=======
   Route::get('/', 'LoginController@index');
->>>>>>> itp-405-laravel
   Route::get('/login', 'LoginController@index');
   Route::post('/login', 'LoginController@login');
   Route::get('/logout', 'LoginController@logout');
-
-<<<<<<< HEAD
   Route::get('/maintenance', 'ConfigurationController@index');
 
   Route::middleware(['authenticated'])->group(function () {
@@ -18,11 +13,10 @@
 
   Route::middleware(['maintenance'])->group(function () {
     Route::get('/', 'GenresController@index');
-=======
+
   Route::get('/maintenance', 'MaintenanceController@index');
 
   Route::middleware(['maintenance'])->group(function(){
->>>>>>> itp-405-laravel
     Route::get('/genres', 'GenresController@index');
     Route::get('/genres/{id}/edit', 'GenresController@edit');
     Route::post('/genres/{id}/store', 'GenresController@store');
@@ -32,12 +26,3 @@
     Route::get('/signup', 'SignUpController@index');
     Route::post('/signup', 'SignUpController@signup');
   });
-<<<<<<< HEAD
-=======
-
-  Route::middleware(['authenticated'])->group(function(){
-    Route::get('/profile', 'AdminController@index');
-    Route::get('/settings', 'SettingController@index');
-    Route::post('/settings/update', 'SettingController@update');
-  });
->>>>>>> itp-405-laravel
